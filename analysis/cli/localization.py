acdab3cdce19179c6398087422931485e58df9df
Calvin Smith <calvin@all-hands.dev>
2025-03-18 09:42:13 -0600

    computing localization data for openhands evaluations

--- analysis/cli/localization.py
+++ analysis/cli/localization.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import click
+from tqdm import tqdm
 
 from analysis.cli.cli import cli
 from analysis.models.patch import Patch
 from analysis.models.localization import Location, LocalizationReport, SystemLocations
 from analysis.models.swe_bench import Split, Dataset
 from analysis.models.data import Data
+from analysis.models.openhands import EvaluationOutput
 
 
 @cli.group()
@@ -28,17 +31,14 @@ def compute_gold(split: Split, output: str) -> None:
     gold_locations: dict[str, list[Location]] = {}
     errors: dict[str, Exception] = {}
 
-    with click.progressbar(
-        dataset.instances, label="Processing instances"
-    ) as instances_bar:
-        for instance in instances_bar:
-            try:
-                patch = Patch.from_instance(instance)
-                locations = patch.locations
-                gold_locations[instance.instance_id] = locations
-            except Exception as e:
-                errors[instance.instance_id] = e
-                continue
+    for instance in tqdm(dataset.instances, desc="Processing instances"):
+        try:
+            patch = Patch.from_instance(instance)
+            locations = patch.locations
+            gold_locations[instance.instance_id] = locations
+        except Exception as e:
+            errors[instance.instance_id] = e
+            continue
 
     click.echo(f"Unable to process {len(errors)} instance(s).")
     for instance_id, error in errors.items():
@@ -74,26 +74,25 @@ def compute_leaderboard(input: str, output: str, error_rate: float) -> None:
 
     systems: list[SystemLocations] = []
 
-    for system, evaluation in data.systems.items():
+    for system, evaluation in tqdm(data.systems.items(), desc="Systems"):
         system_locations = SystemLocations(system=system, locations={})
         errors: dict[str, Exception] = {}
 
-        with click.progressbar(
-            evaluation.predictions, label=f"Processing patches for {system}"
-        ) as predictions_bar:
-            for prediction in predictions_bar:
-                try:
-                    instance = data.dataset[prediction.instance_id]
-                    patch = Patch.from_github(
-                        instance.repo, instance.base_commit, prediction.patch
-                    )
-                    system_locations.locations[prediction.instance_id] = patch.locations
-                except Exception as e:
-                    errors[prediction.instance_id] = e
-                    continue
+        for prediction in tqdm(
+            evaluation.predictions, desc=f"Processing patches for {system}", leave=False
+        ):
+            try:
+                instance = data.dataset[prediction.instance_id]
+                patch = Patch.from_github(
+                    instance.repo, instance.base_commit, prediction.patch
+                )
+                system_locations.locations[prediction.instance_id] = patch.locations
+            except Exception as e:
+                errors[prediction.instance_id] = e
+                continue
 
-                if len(errors) / len(evaluation.predictions) > error_rate:
-                    break
+            if len(errors) / len(evaluation.predictions) > error_rate:
+                break
 
         if len(errors) / len(evaluation.predictions) > error_rate:
             click.echo(f"Too many errors for system {system} ({len(errors)} total).")
@@ -101,6 +100,87 @@ def compute_leaderboard(input: str, output: str, error_rate: float) -> None:
         systems.append(system_locations)
 
     click.echo(f"Writing {len(systems)} instances to {output}...")
+
+    report = LocalizationReport(systems=systems)
+
+    with open(output, "w") as f:
+        f.write(report.model_dump_json())
+
+
+@localization.command()
+@click.argument("input", type=str, nargs=-1)
+@click.option(
+    "--split",
+    "-s",
+    type=Split,
+    default="verified",
+    callback=lambda _ctx, _, value: Split.from_str(value),
+)
+@click.option("--output", "-o", type=str, default="localization.json")
+@click.option("--recursive", "-r", is_flag=True)
+@click.option("--error-rate", "-e", type=float, default=0.1)
+def compute(
+    input: tuple[str, ...],
+    split: Split,
+    output: str,
+    recursive: bool,
+    error_rate: float,
+) -> None:
+    """Compute the localization data from OpenHands evaluations."""
+    # Grab all the system evaluations to be found from the input (recursing if necessary)
+    system_trajectory_paths: dict[str, Path] = {}
+
+    worklist = list(input)
+    while worklist:
+        path = Path(worklist.pop())
+
+        # If the trajectory path exists, add it to the system trajectory paths. We'll parse
+        # it later.
+        trajectory_path = path / "output.jsonl"
+        if trajectory_path.exists():
+            system_trajectory_paths[path.name] = trajectory_path
+
+        # If the trajectory path doesn't exist and we're recursing, extend the worklist
+        # with all subdirectories and we'll eventually repeat the process there.
+        elif recursive:
+            subdirs = [path / subpath for subpath in path.iterdir() if subpath.is_dir()]
+            worklist.extend(subdirs)
+
+    click.echo(f"Found {len(system_trajectory_paths)} system trajectories.")
+
+    dataset = Dataset.from_split(split)
+
+    systems: list[SystemLocations] = []
+
+    for system, path in tqdm(system_trajectory_paths.items(), desc="Systems"):
+        with path.open() as f:
+            outputs = [
+                EvaluationOutput.model_validate_json(line) for line in f.readlines()
+            ]
+
+        system_locations = SystemLocations(system=system, locations={})
+        errors: dict[str, Exception] = {}
+
+        for output in tqdm(outputs, desc=f"Processing patches for {system}", leave=False):
+            try:
+                instance = dataset[output.instance_id]
+                patch_source = output.test_result["git_patch"]
+                patch = Patch.from_github(
+                    instance.repo, instance.base_commit, patch_source
+                )
+                system_locations.locations[output.instance_id] = patch.locations
+
+            except Exception as e:
+                errors[output.instance_id] = e
+                continue
+
+            if len(errors) / len(outputs) > error_rate:
+                break
+
+        if len(errors) / len(outputs) > error_rate:
+            click.echo(f"Too many errors for system {system} ({len(errors)} total).")
+
+        systems.append(system_locations)
 
     report = LocalizationReport(systems=systems)
 
